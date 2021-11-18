404bda2f4c7b2e0f283010966375f41fae6102c0
Michael233ctrl <mishabmv2503@gmail.com>
2021-11-18 12:42:52 +0200

    Database created and populated with data

---
+++ models.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from app import db
+
+
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    surname = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    order = db.relationship('Order', backref='customer', lazy=True)
+
+    def __repr__(self):
+        return f'{self.name} {self.surname}'
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    price = db.Column(db.Float)
+    description = db.Column(db.Text)
+    date_created = db.Column(db.DateTime(timezone=True), default=datetime.utcnow)
+    order = db.relationship('Order', backref='product', lazy=True)
+
+    def __repr__(self):
+        return self.name
+
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=datetime.utcnow)
+
+    def __repr__(self):
+        return str(self.id)
