f04de86be6c89672872a1c80ac4a4d625785fd9e
Michael233ctrl <mishabmv2503@gmail.com>
2021-12-11 18:31:54 +0200

    Add search functionality

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 Customer relationship management (CRM) is a technology for managing all your company’s relationships and interactions
 with customers and potential customers.
 
+[![Coverage Status](https://coveralls.io/repos/github/Michael233ctrl/FlaskCRM/badge.svg?branch=rebuild)](https://coveralls.io/github/Michael233ctrl/FlaskCRM?branch=rebuild)
--- crm/views/homepage_view.py
+++ crm/views/homepage_view.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from crm.models import Customer, Product, Order
 
 home = Blueprint('home', __name__)
@@ -6,9 +6,9 @@ home = Blueprint('home', __name__)
 
 @home.route('/', methods=['GET'])
 def dashboard():
-    customers = Customer.query.all()[:5]
-    products = Product.query.all()[:5]
-    orders = Order.query.all()[:5]
+    customers = Customer.query.limit(5).all()
+    products = Product.query.limit(5).all()
+    orders = Order.query.limit(5).all()
     total_customers = Customer.query.count()
     total_orders = Order.query.count()
     total_products = Product.query.count()
@@ -25,3 +25,15 @@ def dashboard():
     return render_template('dashboard.html', **context)
 
 
+@home.route('/search', methods=['GET'])
+def search():
+    searched = request.args.get('q')
+    if searched:
+        q_customer = Customer.query.filter(
+            Customer.name.ilike(f'%{searched}%') | Customer.surname.ilike(f'%{searched}%')
+        ).all()
+        q_product = Product.query.filter(
+            Product.name.ilike(f'%{searched}%') | Product.description.ilike(f'%{searched}%')
+        ).all()
+        return render_template('search.html', searched=searched, q_customer=q_customer, q_product=q_product)
+    return render_template('search.html')
